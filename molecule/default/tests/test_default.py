c76b25e9a522cf0d0a2a99214991b092fda78533
Sebastien Besson <seb.besson@gmail.com>
2019-10-09 21:46:12 +0200

    Upgrade role testing to Molecule 2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,20 +1,22 @@
+import os
 from datetime import datetime
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_run_backup_script(Command, File, Sudo):
+def test_run_backup_script(host):
     # Note there's a small chance this test will incorrectly fail if it's run
     # run at midnight
     d = datetime.now()
     expected = '/backup/postgresql/database-%s.pgdump' % d.strftime('%Y%m%d')
 
-    with Sudo():
-        out = Command.check_output('/etc/cron.daily/postgresql-backup')
+    with host.sudo():
+        out = host.check_output('/etc/cron.daily/postgresql-backup')
     assert not out
 
-    f = File(expected)
+    f = host.file(expected)
     assert f.is_file
     assert f.size > 2000
